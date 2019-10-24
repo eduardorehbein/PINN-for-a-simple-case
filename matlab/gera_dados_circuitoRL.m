8e8b4dcc99f64e8b39f36e14aa624799f6dcfa52
Eduardo <dudurehbein@hotmail.com>
2019-10-24 15:24:46 -0300

    Matlab simulation

---
+++ matlab/gera_dados_circuitoRL.m
@@ -0,0 +1,29 @@
+% Clears workspace
+clear all
+clc
+
+% Sets parameters
+R = 1e3;
+L = 1e-3;
+
+% Instances data variable
+data = [0 0 0];
+
+for j = 1:100
+    % Resets current and voltage variables
+    i = 0;
+    v = 0;
+
+    % Generates random amplitude and angular frequency
+    v_p = 10*rand(1);
+    omega = 100*rand(1);
+    
+    % Sistem simulation
+    sim('circuito_RL');
+    
+    % Populates data variable
+    data = [data; i.Time i.Data v.Data];
+end
+
+% Prints data to csv
+csvwrite('t_i_v.csv', data);
