dd1ea2b7bd3f335c58bf07c630a74d5ab4b14fb1
Eduardo <dudurehbein@hotmail.com>
2019-10-31 16:07:08 -0300

    Started NN class building

--- example.py
+++ example.py
@@ -1,55 +1,74 @@
 from math import sin
+from pinn import PhysicsInformedNN
 
-from tensorflow import keras
 import tensorflow as tf
 import pandas as pd
 import numpy as np
 
+# Data reading
 df = pd.read_csv('./cleared_t_i_v.csv')
 data = df.values
 
+t0 = []
+i0 = []
+v0 = []
+
+for t, i, v in data:
+    t0.append(t)
+    i0.append(i)
+    v0.append(v)
+
+t0 = np.array(t0)
+i0 = np.array(i0)
+v0 = np.array(v0)
+
+# Instancing PINN
 R = 1000
 L = 0.001
 
-nn = keras.Sequential([
-    keras.layers.Dense(5, activation='tanh', input_shape=(1,)),
-    keras.layers.Dense(5, activation='tanh'),
-    keras.layers.Dense(1, activation='tanh')  # i(t)
-    # keras.layers.Dense(1, activation='custom') f(t, v)
-])
-nn.compile(optimizer='adam',
-              loss='sparse_categorical_crossentropy',
-              metrics=['accuracy'])
+layers = [1, 5, 1]
 
-t = tf.constant(1.0, shape=(1,))
+pinn = PhysicsInformedNN(t0, v0, i0, R, L, layers)
 
-# Links that may help to build the project
-# https://www.tensorflow.org/tutorials/customization/autodiff
-# https://www.tensorflow.org/api_docs/python/tf/gradients
-# https://www.tensorflow.org/guide/eager
-# https://github.com/maziarraissi/PINNs/blob/master/main/continuous_time_inference%20(Schrodinger)/Schrodinger.py
-
-with tf.GradientTape() as gt:
-  z = nn.predict(t)  # The error here occurs because not every operation inside 'predict' is gradient defined
-
-
-# Derivative of z with respect to the original input tensor x
-dz_dx = gt.gradient(z, t)
-
-print(dz_dx)
-
-
-def v_in(t):
-    return sin(100*t)
-
-
-def i(t):
-    return nn.predict([t])
+# nn = keras.Sequential([
+#     keras.layers.Dense(5, activation='tanh', input_shape=(1,)),
+#     keras.layers.Dense(5, activation='tanh'),
+#     keras.layers.Dense(1, activation='tanh')  # i(t)
+#     # keras.layers.Dense(1, activation='custom') f(t, v)
+# ])
+# nn.compile(optimizer='adam',
+#               loss='sparse_categorical_crossentropy',
+#               metrics=['accuracy'])
+#
+# t = tf.constant(1.0, shape=(1,))
+#
+# # Links that may help to build the project
+# # https://www.tensorflow.org/tutorials/customization/autodiff
+# # https://www.tensorflow.org/api_docs/python/tf/gradients
+# # https://www.tensorflow.org/guide/eager
+# # https://github.com/maziarraissi/PINNs/blob/master/main/continuous_time_inference%20(Schrodinger)/Schrodinger.py
+#
+# with tf.GradientTape() as gt:
+#   z = nn.predict(t)  # The error here occurs because not every operation inside 'predict' is gradient defined
+#
+#
+# # Derivative of z with respect to the original input tensor x
+# dz_dx = gt.gradient(z, t)
+#
+# print(dz_dx)
 
 
-def di_dt(t):
-    return t
-
-
-def f(t, v):
-    return di_dt(t) + (R/L)*i(t) - (1/L)*v(t)
+# def v_in(t):
+#     return sin(100*t)
+#
+#
+# def i(t):
+#     return nn.predict([t])
+#
+#
+# def di_dt(t):
+#     return t
+#
+#
+# def f(t, v):
+#     return di_dt(t) + (R/L)*i(t) - (1/L)*v(t)
---
+++ pinn.py
@@ -0,0 +1,143 @@
+import tensorflow as tf
+import numpy as np
+import time
+
+class PhysicsInformedNN:
+    # Initialize the class
+    def __init__(self, t0, v0, i0, R, L, layers):
+
+        # Data for MSEf
+        self.t0_f = t0[50:]
+        self.v0_f = v0[50:]
+
+        # Data for MSEu
+        self.t0 = t0[:50]
+        self.i0 = i0[:50]
+
+        self.R = R  # Resistance
+        self.L = L  # Indutance
+
+        # Initialize NNs
+        self.layers = layers
+        self.weights, self.biases = self.initialize_NN(layers)
+
+        # tf Placeholders
+        self.t0_tf = tf.constant([self.t0], dtype=tf.float32)
+
+        self.i0_tf = tf.constant([self.i0], dtype=tf.float32)
+
+        self.t0_f_tf = tf.constant([self.t0_f], dtype=tf.float32)
+        self.v0_f_tf = tf.constant([self.v0_f], dtype=tf.float32)
+
+        # tf Graphs
+        self.i_pred = self.net_i(tf.slice(self.t0_tf, [0, 0], [1, 1]))
+        self.f_i_pred = self.net_f_i(tf.slice(self.t0_f_tf, [0, 0], [1, 1]), tf.slice(self.t0_f_tf, [0, 0], [1, 1]))
+
+        # Loss
+        self.loss = tf.reduce_mean(tf.square(self.i0_tf - self.i_pred)) + \
+                    tf.reduce_mean(tf.square(self.f_i_pred))
+
+        # Optimizers
+        self.optimizer = tf.opt.ScipyOptimizerInterface(self.loss,
+                                                                method='L-BFGS-B',
+                                                                options={'maxiter': 50000,
+                                                                         'maxfun': 50000,
+                                                                         'maxcor': 50,
+                                                                         'maxls': 50,
+                                                                         'ftol': 1.0 * np.finfo(float).eps})
+
+        self.optimizer_Adam = tf.train.AdamOptimizer()
+        self.train_op_Adam = self.optimizer_Adam.minimize(self.loss)
+
+        # tf session
+        self.sess = tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
+                                                     log_device_placement=True))
+
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+
+    def initialize_NN(self, layers):
+        weights = []
+        biases = []
+        num_layers = len(layers)
+        for l in range(0, num_layers - 1):
+            W = self.xavier_init(size=[layers[l], layers[l + 1]])
+            b = tf.Variable(tf.zeros([1, layers[l + 1]]), dtype=tf.float32)
+            weights.append(W)
+            biases.append(b)
+        return weights, biases
+
+    def xavier_init(self, size):
+        in_dim = size[0]
+        out_dim = size[1]
+        xavier_stddev = np.sqrt(2 / (in_dim + out_dim))
+        return tf.Variable(tf.random.truncated_normal([in_dim, out_dim], stddev=xavier_stddev), dtype=tf.float32)
+
+    def neural_net(self, t, weights, biases):
+        num_layers = len(weights) + 1
+        U = t
+        for l in range(0, num_layers - 2):
+            W = weights[l]
+            b = biases[l]
+            U = tf.tanh(tf.add(tf.matmul(U, W), b))
+        W = weights[-1]
+        b = biases[-1]
+        Y = tf.add(tf.matmul(U, W), b)
+        return Y
+
+    def net_i(self, t):
+        return self.neural_net(t, self.weights, self.biases)
+        # i = []
+        # for j in range(t.shape[0]):
+        #     i.append(self.neural_net(tf.constant([[t[j]]], dtype=tf.float32), self.weights, self.biases))
+        # return tf.stack(i)
+
+    def net_f_i(self, t, v):
+        with tf.GradientTape() as gt:
+            gt.watch(t)
+            i = self.net_i(t)
+        di_dt = gt.gradient(i, t)
+
+        return di_dt + (self.R / self.L) * i - (1 / self.L) * v
+
+    def callback(self, loss):
+        print('Loss:', loss)
+
+    def train(self, nIter):
+
+        tf_dict = {self.x0_tf: self.x0, self.t0_tf: self.t0,
+                   self.u0_tf: self.u0, self.v0_tf: self.v0,
+                   self.x_lb_tf: self.x_lb, self.t_lb_tf: self.t_lb,
+                   self.x_ub_tf: self.x_ub, self.t_ub_tf: self.t_ub,
+                   self.x_f_tf: self.x_f, self.t_f_tf: self.t_f}
+
+        start_time = time.time()
+        for it in range(nIter):
+            self.sess.run(self.train_op_Adam, tf_dict)
+
+            # Print
+            if it % 10 == 0:
+                elapsed = time.time() - start_time
+                loss_value = self.sess.run(self.loss, tf_dict)
+                print('It: %d, Loss: %.3e, Time: %.2f' %
+                      (it, loss_value, elapsed))
+                start_time = time.time()
+
+        self.optimizer.minimize(self.sess,
+                                feed_dict=tf_dict,
+                                fetches=[self.loss],
+                                loss_callback=self.callback)
+
+    def predict(self, X_star):
+
+        tf_dict = {self.x0_tf: X_star[:, 0:1], self.t0_tf: X_star[:, 1:2]}
+
+        u_star = self.sess.run(self.u0_pred, tf_dict)
+        v_star = self.sess.run(self.v0_pred, tf_dict)
+
+        tf_dict = {self.x_f_tf: X_star[:, 0:1], self.t_f_tf: X_star[:, 1:2]}
+
+        f_u_star = self.sess.run(self.f_u_pred, tf_dict)
+        f_v_star = self.sess.run(self.f_v_pred, tf_dict)
+
+        return u_star, v_star, f_u_star, f_v_star
