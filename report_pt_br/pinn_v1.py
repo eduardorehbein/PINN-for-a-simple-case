f3ee7ee75be4b972b619c3649da4ec274247a44d
Eduardo Rehbein <dudurehbein@hotmail.com>
2020-01-15 10:09:02 -0300

    Report simulation

---
+++ report_pt_br/pinn_v1.py
@@ -0,0 +1,97 @@
+import tensorflow as tf
+import numpy as np
+
+# TODO: Improve it basing on https://github.com/pierremtb/PINNs-TF2.0/blob/master/utils/neuralnetwork.py
+
+
+class CircuitPINN:
+    def __init__(self, R, L, hidden_layers, learning_rate):
+        # Circuit parameters
+        self.R = R  # Resistance
+        self.L = L  # Inductance
+
+        # Initialize NN
+        self.layers = [1] + hidden_layers + [1]
+        self.weights, self.biases = self.initialize_NN(self.layers)
+
+        # Optimizer
+        self.optimizer = tf.optimizers.Adam(learning_rate=learning_rate)
+
+    def initialize_NN(self, layers):
+        weights = []
+        biases = []
+        num_layers = len(layers)
+        for l in range(1, num_layers):
+            W = self.xavier_init(size=[layers[l], layers[l - 1]])
+            b = tf.Variable(tf.zeros([layers[l], 1]), dtype=tf.float32)
+            weights.append(W)
+            biases.append(b)
+
+        return weights, biases
+
+    def xavier_init(self, size):
+        in_dim = size[0]
+        out_dim = size[1]
+        xavier_stddev = np.sqrt(2 / (in_dim + out_dim))
+
+        return tf.Variable(tf.random.truncated_normal([in_dim, out_dim], stddev=xavier_stddev), dtype=tf.float32)
+
+    def predict(self, numpy_or_list_t):
+        t = tf.constant(numpy_or_list_t, dtype=tf.float32)
+
+        return self.i(t)
+
+    def i(self, t):
+        num_layers = len(self.weights) + 1
+        U = t
+        for l in range(0, num_layers - 2):
+            W = self.weights[l]
+            b = self.biases[l]
+            U = tf.tanh(tf.add(tf.matmul(W, U), b))
+        W = self.weights[-1]
+        b = self.biases[-1]
+        Y = tf.add(tf.matmul(W, U), b)
+
+        return Y
+
+    def f(self, t, v):
+        # ODE: Ldi_dt = v - Ri
+        with tf.GradientTape() as gtf:
+            gtf.watch(t)
+            i = self.i(t)
+        di_dt = gtf.gradient(i, t)
+
+        return di_dt + (self.R / self.L) * i - (1 / self.L) * v
+
+    def train(self, train_t, train_i, train_v, epochs=1, train_f_percent=0.99):
+        # Data for f loss
+        train_f_index = int(train_f_percent * train_t.shape[1])
+        f_t_train = tf.constant(train_t[:, :train_f_index], dtype=tf.float32)
+        f_v_train = tf.constant(train_v[:, :train_f_index], dtype=tf.float32)
+
+        # Data for u loss
+        u_t_train = tf.constant(train_t[:, train_f_index:], dtype=tf.float32)
+        u_i_train = tf.constant(train_i[:, train_f_index:], dtype=tf.float32)
+
+        for j in range(epochs):
+            # Gradients
+            grad_weights, grad_biases = self.get_grads(f_t_train, f_v_train, u_i_train, u_t_train)
+
+            # Updating weights and biases
+            grads = grad_weights + grad_biases
+            vars_to_update = self.weights + self.biases
+            self.optimizer.apply_gradients(zip(grads, vars_to_update))
+
+    def get_grads(self, f_t_train, f_v_train, u_i_train, u_t_train):
+        with tf.GradientTape(persistent=True) as gtu:
+            u_i_predict = self.i(u_t_train)
+            u_loss = tf.reduce_mean(tf.square(u_i_predict - u_i_train))
+
+            f_predict = self.f(f_t_train, f_v_train)
+            f_loss = tf.reduce_mean(tf.square(f_predict))
+
+            total_loss = u_loss + f_loss
+        grad_weights = gtu.gradient(total_loss, self.weights)
+        grad_biases = gtu.gradient(total_loss, self.biases)
+
+        return grad_weights, grad_biases
--- report_pt_br/simulacao.py
+++ report_pt_br/simulacao.py
@@ -2,28 +2,24 @@ from pinn_v1 import CircuitPINN
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 # Leitura de dados
-df = pd.read_csv('../matlab/noisy_t_i_v_v3.csv')
+df1 = pd.read_csv('../matlab/t_i_v_v3.csv')
+df2 = pd.read_csv('../matlab/noisy_t_i_v_v3.csv').drop(columns=['t', 'v'])
+df = pd.concat([df1, df2], axis=1, sort=False)
+print(df)
 shuffled_df = df.sample(frac=1)
 
 # Separação dos dados de treino e teste
 test_percent = 0.2
 test_data_len = int(test_percent * len(shuffled_df))
 test_shuffled_df = shuffled_df.sample(n=test_data_len)
+
 train_shuffled_df = shuffled_df[~shuffled_df.isin(test_shuffled_df)].dropna()
-
-# Preparação dos dados reais e simulados
-train_u_percent = 0.01
-u_data_len = int(train_u_percent * len(train_shuffled_df))
-u_df = train_shuffled_df.sample(n=u_data_len)
-f_df = train_shuffled_df[~train_shuffled_df.isin(u_df)].dropna()
-
-# Conversão para dados numpy
-np_u_t = u_df['t'].values
-np_u_i = u_df['noisy_i'].values
-np_f_t = f_df['t'].values
-np_f_v = f_df['v'].values
+train_t = np.array([train_shuffled_df['t'].values])
+train_i = np.array([train_shuffled_df['noisy_i'].values])
+train_v = np.array([train_shuffled_df['v'].values])
 
 # Instanciando PINN
 R = 3
@@ -34,17 +30,17 @@ model = CircuitPINN(R, L, hidden_layers, learning_rate)
 
 # Treino e teste
 epochs = 15000
-model.train(np_u_t, np_u_i, np_f_t, np_f_v, epochs)
+model.train(train_t, train_i, train_v, epochs)
 
-sorted_test_df = test_shuffled_df.sort(['t'])
-np_test_t = sorted_test_df['t'].values
+sorted_test_df = test_shuffled_df.sort_values(['t'])
+np_test_t = np.array([sorted_test_df['t'].values])
 np_test_i = sorted_test_df['i'].values
 
 np_prediction = model.predict(np_test_t)
 
 # Plotando resultados
 plt.title('Comparação nn(t) e i(t)')
-plt.plot(np_test_t, np_test_i, label='i(t)')
-plt.plot(np_test_t, np_prediction, label='nn(t)')
+plt.plot(np_test_t[0], np_test_i, label='i(t)')
+plt.plot(np_test_t[0], np_prediction[0], label='nn(t)')
 plt.legend()
 plt.show()
